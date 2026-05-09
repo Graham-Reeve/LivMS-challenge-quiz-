63ca9d10391e64f06a6812053c81c5fa05307ba8
Graham-Reeve <grahamreeve86@gmail.com>
2026-05-09 14:22:42 +0100

    Update quiz configuration with new endpoints and admin key

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.QUIZ_CONFIG = {
   title: "Liverpool Maths Society Challenge Quiz",
   subtitle: "Enter your name, answer 20 questions, and see how many you get correct.",
-  resultsEndpoint: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
-  leaderboardEndpoint: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
-  adminKey: "CHANGE_ME_TO_A_PRIVATE_SECRET",
+  resultsEndpoint: "https://script.google.com/macros/s/AKfycbzXW5p1eid9FvvRuINLGSJadF3EcmmxhhixLD93PhHGG2EiI02WozRfu7JY0RG2dkNX/exec",
+  leaderboardEndpoint: "https://script.google.com/macros/s/AKfycbzXW5p1eid9FvvRuINLGSJadF3EcmmxhhixLD93PhHGG2EiI02WozRfu7JY0RG2dkNX/exec",
+  adminKey: "Gauss1777",
   competitionYear: "2026",
   wholeNumbersOnly: true
 };
